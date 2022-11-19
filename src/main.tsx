6284d6fc6bd52fb7d731e8d6ec5b2f63866e06c1
ElijoaoGarcia <57513172+ElijoaoGarcia@users.noreply.github.com>
2022-11-19 13:28:55 -0400

    Update main.tsx
    add suspense and lazy loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Fallback from './components/Fallback'
 import './index.css'
+
+const App = lazy(async () => await import('./App'))
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<Fallback />}>
+      <App />
+    </Suspense>
   </React.StrictMode>
 )
